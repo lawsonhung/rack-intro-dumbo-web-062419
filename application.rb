db0799e56941d0ca48a8cb51cd3751aba0f8bf17
github username <github email address>
2019-07-14 20:12:33 -0400

    Done

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World I am a student at Flatiron"
+    resp.write "Hello, World I am a student at Flatiron. Hello, my name is Ada Lovelace, the first computer programmer in history! Grace Hopper found a moth in her computer which coined the term bug!"
     resp.finish
   end
 
